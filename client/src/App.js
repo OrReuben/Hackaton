6371ed30df1a975da3ad276a57f6ecaf4b4cf6f3
OrReuben <orreuven1243@gmail.com>
2022-09-21 14:09:55 +0300

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import Todo from './components/Todo';
 import './app.css';
 const App = () => {
   return (
     <div className="App">
-      <Todo />
+      test
     </div>
   );
 };
