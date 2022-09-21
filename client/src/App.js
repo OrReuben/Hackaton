249d183641c18a7dbbc1ee61244ce4bacdde302e
Nir Levin <niro@Nirs-MacBook-Pro.local>
2022-09-21 18:58:40 +0300

    cards

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './app.css';
+
+import MainDash from './components/MAIN-DASH/MainDash';
+
 const App = () => {
   return (
-    <div className="App">
-      test
+    <div className='Home'>
+      <div className='home-content'>
+        <MainDash />
+      </div>
     </div>
   );
 };
---
+++ client/src/components/MAIN-DASH/MainDash.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./main-dash.css"
+
+import Cards from "../CARDS/Cards";
+
+const MainDash = () => {
+
+
+    return (
+        <div className="MainDash">
+            <h1>DASH BOARD</h1>
+            <Cards />
+        </div>
+    )
+} 
+
+export default MainDash;
