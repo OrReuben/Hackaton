dd26ac7fbbb4c8c988004b8ef73eda90f0dbc4be
OrReuben <orreuven1243@gmail.com>
2022-09-21 20:47:30 +0300

    merged together

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './app.css';
 import MainDash from './components/MainDash/MainDash';
+import Sidebar  from './components/Sidebar'
 const App = () => {
   return (
     <div className="App">
       <div className="AppGlass">
-        <h1>test</h1>
+      <Sidebar />
       <MainDash />
         <h1>test</h1>
       </div>
--- client/src/components/MainDash/MainDash.jsx
+++ client/src/components/MainDash/MainDash.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import Cards from "../Cards/Cards";
 import BasicTable from "../Table/Table";
-
+import "./MainDash.css";
 const MainDash = () => {
-    return (
-        <div>
-        <BasicTable />
-        </div>
-    )
-}
+  return (
+    <div className="MainDash">
+      <h3 style={{textAlign:"right"}}>נתונים</h3>
+      <Cards />
+      <BasicTable />
+    </div>
+  );
+};
 
-export default MainDash
+export default MainDash;
